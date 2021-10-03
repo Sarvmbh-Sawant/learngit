274b56f3894f5843965cb11980beb211a923b172
Sarvmbh-Sawant <sarvmbh742@gmail.com>
2021-10-03 21:20:47 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+
+    public static void main(String[] args) {
+         
+       int a = 11;
+       int b = 11;
+ 
+       System.out.println("The product is " (a*b));
+    }
+ } 
